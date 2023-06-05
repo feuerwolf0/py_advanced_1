e7f6f287ab960ea04e3d00611fae4eae0f1653eb
Poletaev Kirill <poletaev777@gmail.com>
2023-06-05 20:44:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+from datetime import datetime
+
+from art import text2art, aprint
+
+
+def text_to_image(text):
+    # Функция создает из переданого текста (Eng) изображение используя символы ASCII
+    # и печатает результат
+    print(text2art(text))
+
+def draw_random():
+    # Функция печатает случайный смайлик/изображение символами ASCII
+    aprint('random')
+
+if __name__=='__main__':
+    print('Текущая дата и время:',datetime.now())
+    calculate_salary()
+    get_employees()
+
+    text_to_image('ALIEN')
+    draw_random()
